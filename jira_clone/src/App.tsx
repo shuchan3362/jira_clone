07f4e00ab9ec63479250f61b65831863f0f8c5e9
Shusuke Saito <jikojiko931@gmail.com>
2021-07-22 23:33:47 +0900

    main

--- jira_clone/src/App.tsx
+++ jira_clone/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload
         </p>
-        <Hanakuso hanakuso={{ haha: "unmo" }} />
+        <Hanakuso hanakuso={{ haha: "unmohahahahahaha" }} />
       </header>
     </div>
   );
--- jira_clone/src/hanakuso/Haaaa.tsx
+++ jira_clone/src/hanakuso/Haaaa.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type Props = {
   hanakuso?: {
     haha: string;
